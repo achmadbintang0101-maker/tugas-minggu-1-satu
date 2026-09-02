6c9194621168c7ad2e3a1ecad8f5d275fe06eeaa
achmadbintang0101-maker <achmadbintang0101.com>
2026-09-02 09:28:03 +0700

    folder tempat latihan dart

---
+++ apps/lib/Bintang G/integer.dart
@@ -0,0 +1,14 @@
+void main() {
+  int a = 10;
+  int b = 3;
+
+  int sum = a + b;       // 13
+  int difference = a - b; // 7
+  int product = a * b;   // 30
+  
+  int division = a ~/ b; // 3 (Integer division drops the decimal)
+  int remainder = a % b; // 1 (Modulo)
+  
+  a++;                   // 11 (Increment)
+  b--;                   // 2  (Decrement)
+}
---
+++ apps/lib/elando/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Extra Joss : Samzen',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepOrange)),
+      home: const MyHomePage(title: 'Flutter demo home'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text('iyawes'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: const Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.green,
+          ),
+        ],
+      ),
+    );
+  }
+}
